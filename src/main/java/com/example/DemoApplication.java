0e38d75e2ce58d34536c666fc1c8a7303249a1a8
Bruno Oliveira <bruno.oliveira@mdcpartners.be>
2019-01-21 12:02:04 +0100

    Optimize imports

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,20 +1,11 @@
 package com.example;
 
-import com.example.repository.TodoRepository;
-import com.example.repository.UserRepository;
-import com.example.todoitem.TodoItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
